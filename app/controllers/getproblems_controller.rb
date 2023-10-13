eaee17c79e608d0307580ac7f657387ae845bb9c
Nakka0925 <h23ms413@hirosaki-u.ac.jp>
2023-10-14 05:31:20 +0900

    Add filtered

--- app/controllers/getproblems_controller.rb
+++ app/controllers/getproblems_controller.rb
@@ -4,7 +4,17 @@ class GetproblemsController < ApplicationController
 
   def create
     entry = GetProblem.new()
-    session[:res_data] = entry.resoponse_problem()
+    a = params[:a].to_i
+    b = params[:b].to_i
+    c = params[:c].to_i
+    d = params[:d].to_i
+    e = params[:e].to_i
+    f = params[:f].to_i
+    g = params[:g].to_i
+    ex = params[:ex].to_i
+
+
+    session[:res_data] = entry.resoponse_problem(a,b,c,d,e,f,g,ex)
     session[:contest_id] = session[:res_data][0]["contest_id"]
     session[:problem_id] = session[:res_data][0]["problem_id"]
     session[:problem_index] = session[:res_data][0]["problem_index"]
--- app/models/get_problem.rb
+++ app/models/get_problem.rb
@@ -1,17 +1,28 @@
 require 'json'
 
 class GetProblem < ApplicationRecord
-    def get_random_contest()
-        file_path = '/atcoder_app/contest.json'
+    def get_random_contest(parsed_data,a,b,c,d,e,f,g,ex)
+        filtered_data = parsed_data.select do |problem|
+            (a == 1 && problem["problem_index"] == "A") ||
+            (b == 1 && problem["problem_index"] == "B") ||
+            (c == 1 && problem["problem_index"] == "C") ||
+            (d == 1 && problem["problem_index"] == "D") ||
+            (e == 1 && problem["problem_index"] == "E") ||
+            (f == 1 && problem["problem_index"] == "F") ||
+            (g == 1 && problem["problem_index"] == "G") ||
+            (ex == 1 && problem["problem_index"] == "Ex") 
+        end
 
-        json_data = File.read(file_path)
-        parsed_data = JSON.parse(json_data)
-        
-        return parsed_data.sample
+        if filtered_data.empty?
+            return parsed_data.sample
+        end
+
+        random_problem = filtered_data.sample
+        return random_problem
     end
 
 
-    def resoponse_problem()
+    def resoponse_problem(a,b,c,d,e,f,g,ex)
         file_path = "problem.json"
 
         begin
@@ -28,7 +39,8 @@ class GetProblem < ApplicationRecord
             puts "Error parsing JSON: #{e.message}"
         end
 
-        target_problem_id = get_random_contest()
+        target_problem_id = get_random_contest(parsed_data,a,b,c,d,e,f,g,ex)
+
 
         # 指定したcontest_idに対応するproblem_idとproblem_indexを格納する配列
         result = []
@@ -36,7 +48,7 @@ class GetProblem < ApplicationRecord
         # データから指定したcontest_idに対応するproblem_idとproblem_indexを抽出
 
         parsed_data.each do |problem|
-            if problem["problem_id"] == target_problem_id
+            if problem["problem_id"] == target_problem_id["problem_id"]
                 result << problem
             end
         end
