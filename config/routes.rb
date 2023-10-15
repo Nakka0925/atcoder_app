e1846a245beea2accdf9dcb36d96848fd8442bb2
uip-kleh <h23ms416@hirosaki-u.ac.jp>
2023-10-15 16:14:54 +0900

    add signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'getproblems#top'
+  get 'users/new'
+  root to: 'getproblems#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # ログイン周り
+  get '/signup', to: 'users#new'
+
+  resources :users
 
   # Defines the root path route ("/")
   # root "articles#index"
