04386af0f0a98c56790f7646723f6a81aeda927b
uip-kleh <h23ms416@hirosaki-u.ac.jp>
2023-10-15 13:48:52 +0900

    make user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_02_092503) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_15_044605) do
   create_table "get_problems", charset: "utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
