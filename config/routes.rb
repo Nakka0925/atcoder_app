b99ab8bc29c528786cb75fcf1a2cc3be7987cb13
uip-kleh <h23ms416@hirosaki-u.ac.jp>
2023-10-15 17:08:10 +0900

    add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root to: 'getproblems#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # ログイン周り
   get '/signup', to: 'users#new'
+  get '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
 
