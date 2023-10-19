f2033f3b1d0a9d0d6f49da6234abec88b3c04219
Nakka0925 <h23ms413@hirosaki-u.ac.jp>
2023-10-20 01:00:58 +0900

    Register to Detabase from csv file

--- lib/import_csv.rb
+++ lib/import_csv.rb
@@ -13,7 +13,5 @@ class ImportCsv
     end
     # 与えられたモデルに CSVデータを投入
     model_name.constantize.import!(list, on_duplicate_key_update: :all)
-    # 次に振る id を正常化
-    ActiveRecord::Base.connection.execute("select setval('#{table_name}_id_seq',(select max(id) from #{table_name}))")
   end
 end
