0bb0328bc937e0a69ed372be50d6376d035d2813
Nakka0925 <h23ms413@hirosaki-u.ac.jp>
2023-10-21 20:03:03 +0900

    Add get radom algo problem

--- app/controllers/getproblems_controller.rb
+++ app/controllers/getproblems_controller.rb
@@ -3,13 +3,20 @@ class GetproblemsController < ApplicationController
     @algo_names = Algo.pluck(:algo_name)
   end
 
-  def create
+  def random_problem
     entry = GetProblem.new()
 
     session[:res_data] = entry.resoponse_problem(params)
     session[:contest_id] = session[:res_data][0]["contest_id"]
     session[:problem_id] = session[:res_data][0]["id"]
     session[:problem_index] = session[:res_data][0]["problem_index"]
+    redirect_to root_path
+  end
+
+  def algo_problem
+    name_to_id = Algo.where(algo_name: params[:algo_test])[0][:algo_id]
+    session[:algo] = Problem.where(algo_id: name_to_id).sample
+
     redirect_to root_path
   end
 end
