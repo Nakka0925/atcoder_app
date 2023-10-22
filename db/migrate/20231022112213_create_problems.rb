eb596df0f7ecce61bcfca702ec432b3d2e590637
Nakka0925 <h23ms413@hirosaki-u.ac.jp>
2023-10-22 21:05:15 +0900

    Modify problems table

--- db/migrate/20231022112213_create_problems.rb
+++ db/migrate/20231022112213_create_problems.rb
@@ -1,8 +1,12 @@
 class CreateProblems < ActiveRecord::Migration[7.0]
   def change
-    create_table :problems do |t|
-      t.string :problem_id
+    create_table :problems, id: false do |t|
+      t.string :id
+      t.string :contest_id
+      t.string :problem_index
+      t.string :name
       t.bigint :algo_id
+
 
       t.timestamps
     end
--- get_ploblem_data.rb
+++ get_ploblem_data.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'json'
 require 'uri'
+require 'csv'
 
 uri = URI.parse('https://kenkoooo.com/atcoder/resources/merged-problems.json')
 
@@ -15,11 +16,32 @@ begin
     end
 
     puts "JSONデータがproblem.jsonに正常に保存されました"
-
-    contests = []
-    json_data.each do |contents|
-      contests.push(contents["problem_id"])
+    algo = CSV.read("db/csv_data/problem_to_algo.csv",headers: true)
+    problem_id = []
+    
+    algo.each do |ary|
+      problem_id.push(ary[0])
     end
+    
+    csv_data = CSV.generate do |csv|
+      # ヘッダー行を追加
+      csv << json_data.first.keys.slice(..3).push("algo_id")
+      # データ行を追加
+      json_data.each do |hash|
+        idx = problem_id.index(hash.values[0])
+        if idx != nil
+          csv << hash.values.slice(..3).push(algo[idx][1])
+        else
+          csv << hash.values.slice(..3).push(nil)
+        end 
+      end
+    end
+    
+    # CSVデータをファイルに書き込む
+    File.open('db/csv_data/problem.csv', 'w') do |file|
+      file.write(csv_data)
+    end
+    puts 'CSVファイルに保存しました。'
     
   else
     puts "データの取得に失敗しました。HTTPレスポンスコード:#{response.code}"
