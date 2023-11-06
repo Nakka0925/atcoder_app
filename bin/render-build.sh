3d5fa367309e9ee188c7435fbf1cbe0b15de07cc
nakanishi <h19s4040@hirosaki-u.ac.jp>
2023-11-06 21:41:20 +0900

    Remove yarn build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 
-bundle lock --add-platform x86_64-linux
+# bundle lock --add-platform x86_64-linux
 bundle install
-yarn install
-yarn build # jsファイルをesbuildでバンドルしているため
+# yarn install
+# yarn build # jsファイルをesbuildでバンドルしているため
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
