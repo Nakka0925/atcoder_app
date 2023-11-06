a640a7f031effd862fc287d6b3ed1890821a251b
nakka <105539012+Nakka0925@users.noreply.github.com>
2023-11-06 21:48:46 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,6 @@ set -o errexit
 bundle install
 # yarn install
 # yarn build # jsファイルをesbuildでバンドルしているため
-bundle exec rake assets:precompile
+bundle exec rake assets:precompile --trace
 bundle exec rake assets:clean
 bundle exec rake db:migrate
