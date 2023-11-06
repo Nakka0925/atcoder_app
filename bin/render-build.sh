398544658c810d9898bf35dd1135be1342f281e0
Nakka0925 <h23ms413@hirosaki-u.ac.jp>
2023-11-07 03:47:58 +0900

    Modify render shell

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,10 +2,9 @@
 # exit on error
 set -o errexit
 
-# bundle lock --add-platform x86_64-linux
 bundle install
 # yarn install
 # yarn build # jsファイルをesbuildでバンドルしているため
-bundle exec rake assets:precompile --trace
+bundle exec rake assets:precompile 
 bundle exec rake assets:clean
-bundle exec rake db:migrate --trace
+bundle exec rake db:create
