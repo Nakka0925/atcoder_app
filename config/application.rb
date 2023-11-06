9462690986e8dcb3eb5177b8172321d484e72409
nakanishi <h19s4040@hirosaki-u.ac.jp>
2023-11-06 21:25:28 +0900

    Fix deploy error

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module AtcoderApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "https://atcoder-app.onrender.com"
   end
 end
