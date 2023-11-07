8b2856eb6061fd020bf983a920c787e8718e4a73
nakanishi <h19s4040@hirosaki-u.ac.jp>
2023-11-07 19:04:10 +0900

    Modify PlanetScale table

--- db/migrate/20231005080855_create_users.rb
+++ db/migrate/20231005080855_create_users.rb
@@ -1,6 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    drop_table :problems
     create_table :users do |t|
       t.string :name, null: false
       t.string :email, null: false
